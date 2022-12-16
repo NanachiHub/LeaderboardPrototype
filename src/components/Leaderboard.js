34e40a80265680348a65424c690de1129186310e
Nanachi <70123136+NanachiHub@users.noreply.github.com>
2022-12-16 16:18:59 +1100

    Add selectors

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,8 +1,17 @@
 import React, { useState } from 'react';
-import { Switch, Table } from 'antd';
+import { Table } from 'antd';
 import LeaderboardCol from './Data/LeaderboardCol';
+import { Select, Space } from 'antd';
+
+const onChange = (value) => {
+  console.log(`selected ${value}`);
+};
+const onSearch = (value) => {
+  console.log('search:', value);
+};
 
 const data = [];
+
 for (let i = 0; i < 100; i++) {
   data.push({
     key: i,
@@ -15,14 +24,90 @@ for (let i = 0; i < 100; i++) {
 function Leaderboard() {
   const [fixedTop, setFixedTop] = useState(false);
   return (
-    <Table
-      columns={LeaderboardCol}
-      dataSource={data}
-      scroll={{
-        x: 1500,
-      }}
-      sticky
-    />
+    <>
+      <Space>
+        <Select
+          showSearch
+          placeholder="Select a language"
+          optionFilterProp="children"
+          onChange={onChange}
+          onSearch={onSearch}
+          filterOption={(input, option) =>
+            (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+          }
+          options={[
+            {
+              value: 'en',
+              label: 'English',
+            },
+            {
+              value: 'zh-chs',
+              label: 'Simplified Chinese',
+            },
+            {
+              value: 'de',
+              label: 'German',
+            },
+          ]}
+        />
+        <Select
+          showSearch
+          placeholder="Select a dimension"
+          optionFilterProp="children"
+          onChange={onChange}
+          onSearch={onSearch}
+          filterOption={(input, option) =>
+            (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+          }
+          options={[
+            {
+              value: 'd1',
+              label: 'd1',
+            },
+            {
+              value: 'd2',
+              label: 'd2',
+            },
+            {
+              value: 'd3',
+              label: 'd3',
+            },
+          ]}
+        />
+        <Select
+          showSearch
+          placeholder="Select a task"
+          optionFilterProp="children"
+          onChange={onChange}
+          onSearch={onSearch}
+          filterOption={(input, option) =>
+            (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+          }
+          options={[
+            {
+              value: 't1',
+              label: 't1',
+            },
+            {
+              value: 't2',
+              label: 't2',
+            },
+            {
+              value: 't3',
+              label: 't3',
+            },
+          ]}
+        />
+      </Space>
+      <Table
+        columns={LeaderboardCol}
+        dataSource={data}
+        scroll={{
+          x: 1500,
+        }}
+        sticky
+      />
+    </>
   );
 };
   
